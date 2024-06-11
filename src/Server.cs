e4dddf9da60ef47b1549a785993ca6bc85afedc0
Kristijan Jovanovski <kristijan.jovanovski@pcg.io>
2024-06-11 13:05:26 +0200

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -8,46 +8,61 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-var socket = server.AcceptSocket(); // wait for client
-
 Byte[] bytes = new Byte[256];
-socket.Receive(bytes);
 
-var data = Encoding.UTF8.GetString(bytes) ?? "";
-
-var reqDataChunks = data?.Split("\r\n");
-
-// Status line like GET /echo/abc HTTP/1.1
-var statusLine = reqDataChunks?[0];
-
-// Route from status line like /echo/abc
-string route = statusLine?.Split(" ")?[1].Trim()!;
-
-Console.WriteLine("THIS IS MY data: {0}", data);
-
-if (route == "/user-agent")
+try
 {
-  var userAgent = reqDataChunks?[2].Replace("User-Agent: ", "");
-  socket.Send(Encoding.UTF8.GetBytes(
-    "HTTP/1.1 200 OK\r\n" +
-    $"Content-Type: text/plain\r\nContent-Length: {userAgent?.Length}\r\n\r\n" +
-    userAgent
-  ));
+  while (true)
+  {
+    var socket = server.AcceptSocket(); // wait for client
+    socket.Receive(bytes);
+
+    var data = Encoding.UTF8.GetString(bytes) ?? "";
+
+    var reqDataChunks = data?.Split("\r\n");
+
+    // Status line like GET /echo/abc HTTP/1.1
+    var statusLine = reqDataChunks?[0];
+
+    // Route from status line like /echo/abc
+    string route = statusLine?.Split(" ")?[1].Trim()!;
+
+    Console.WriteLine("THIS IS MY data: {0}", data);
+
+    if (route == "/user-agent")
+    {
+      var userAgent = reqDataChunks?[2].Replace("User-Agent: ", "");
+      socket.Send(Encoding.UTF8.GetBytes(
+        "HTTP/1.1 200 OK\r\n" +
+        $"Content-Type: text/plain\r\nContent-Length: {userAgent?.Length}\r\n\r\n" +
+        userAgent
+      ));
+    }
+    else if (route.Contains("/echo/"))
+    {
+      var responseString = route.Replace("/echo/", "");
+      socket.Send(Encoding.UTF8.GetBytes(
+        "HTTP/1.1 200 OK\r\n" +
+        $"Content-Type: text/plain\r\nContent-Length: {responseString.Length}\r\n\r\n" +
+        responseString
+      ));
+    }
+    else if (route == "/")
+    {
+      socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
+    }
+    else
+    {
+      socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n"));
+    }
+  }
 }
-else if (route.Contains("/echo/"))
+catch (SocketException e)
 {
-  var responseString = route.Replace("/echo/", "");
-  socket.Send(Encoding.UTF8.GetBytes(
-    "HTTP/1.1 200 OK\r\n" +
-    $"Content-Type: text/plain\r\nContent-Length: {responseString.Length}\r\n\r\n" +
-    responseString
-  ));
+  Console.WriteLine("Error connecting: {0}", e.Message);
+  throw;
 }
-else if (route == "/")
+finally
 {
-  socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
-}
-else
-{
-  socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n"));
+  server.Stop();
 }
